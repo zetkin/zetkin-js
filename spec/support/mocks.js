1c0826e34ce44c744210cdf18b98893fd4579be1
Richard Olsson <r@richardolsson.se>
2015-07-10 11:58:06 +0200

    Create versatile request mocking
    * Spies on http.request / https.request as specified by the test spec
    * Simulates HTTP server to respond with the specified status code and content
    * Calls all the expected callbacks et c
    * Allows for extra expectations on http client options

---
+++ spec/support/mocks.js
@@ -0,0 +1,37 @@
+var http = require('http');
+
+var spyOnRequest = function(client, method, path,
+                            responseCode, responseContent, extraExpects) {
+
+    spyOn(client, 'request').and.callFake(function(options, cb) {
+        expect(options.hostname).toBe('api.zetk.in');
+        expect(options.port).toBe(80);
+        expect(options.method).toBe(method);
+        expect(options.path).toBe(path);
+
+        if (extraExpects) {
+            extraExpects(options);
+        }
+
+        var res = new http.IncomingMessage();
+        res.statusCode = responseCode || 200;
+        cb(res);
+
+        // Mock request
+        return {
+            on: function(ev, func) {},
+            end: function() {
+                if (responseContent) {
+                    res.emit('data', responseContent);
+                }
+
+                res.emit('end');
+            }
+        };
+    });
+};
+
+
+module.exports = {
+    spyOnRequest: spyOnRequest
+};
